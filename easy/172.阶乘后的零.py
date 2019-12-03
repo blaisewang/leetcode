944c1eb8a717af431db26fac20b16842667afd3f
blaisewang <kaito.kaito.k@gmail.com>
2019-12-04 02:15:33 +0800

    Remove redundant spaces

--- easy/172.阶乘后的零.py
+++ easy/172.阶乘后的零.py
@@ -17,30 +17,28 @@
 # 
 # 示例 1:
 # 
-# 输入: 3
+# 输入: 3
 # 输出: 0
-# 解释: 3! = 6, 尾数中没有零。
+# 解释: 3! = 6, 尾数中没有零。
 # 
-# 示例 2:
+# 示例 2:
 # 
 # 输入: 5
 # 输出: 1
-# 解释: 5! = 120, 尾数中有 1 个零.
+# 解释: 5! = 120, 尾数中有 1 个零.
 # 
-# 说明: 你算法的时间复杂度应为 O(log n) 。
+# 说明: 你算法的时间复杂度应为 O(log n) 。
 # 
 #
 
 # @lc code=start
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-
-        p = 0 
-        while n >= 5: 
-            n = n // 5 
+        p = 0
+        while n >= 5:
+            n = n // 5
             p += n
 
         return p
-        
-# @lc code=end
 
+# @lc code=end
