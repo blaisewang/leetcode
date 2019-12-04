a1f5daf9ee0b240dbb5d9ef729b4db31d219cb81
blaisewang <kaito.kaito.k@gmail.com>
2019-12-05 04:03:49 +0800

    Solve 303, 326, 342, 344-345, 349-350, 367, 371, 374

---
+++ easy/342.4-的幂.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode.cn id=342 lang=python3
+#
+# [342] 4的幂
+#
+# https://leetcode-cn.com/problems/power-of-four/description/
+#
+# algorithms
+# Easy (46.81%)
+# Likes:    73
+# Dislikes: 0
+# Total Accepted:    15.4K
+# Total Submissions: 32.8K
+# Testcase Example:  '16'
+#
+# 给定一个整数 (32 位有符号整数)，请编写一个函数来判断它是否是 4 的幂次方。
+# 
+# 示例 1:
+# 
+# 输入: 16
+# 输出: true
+# 
+# 
+# 示例 2:
+# 
+# 输入: 5
+# 输出: false
+# 
+# 进阶：
+# 你能不使用循环或者递归来完成本题吗？
+# 
+#
+
+# @lc code=start
+class Solution:
+    def isPowerOfFour(self, num: int) -> bool:
+
+        if num <= 0 or num & num - 1:
+            return False
+
+        return (num & 0x55555555) != 0
+
+# @lc code=end
---
+++ easy/371.两整数之和.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode.cn id=371 lang=python3
+#
+# [371] 两整数之和
+#
+# https://leetcode-cn.com/problems/sum-of-two-integers/description/
+#
+# algorithms
+# Easy (52.55%)
+# Likes:    165
+# Dislikes: 0
+# Total Accepted:    18.6K
+# Total Submissions: 35.1K
+# Testcase Example:  '1\n2'
+#
+# 不使用运算符 + 和 - ​​​​​​​，计算两整数 ​​​​​​​a 、b ​​​​​​​之和。
+# 
+# 示例 1:
+# 
+# 输入: a = 1, b = 2
+# 输出: 3
+# 
+# 
+# 示例 2:
+# 
+# 输入: a = -2, b = 3
+# 输出: 1
+# 
+#
+
+# @lc code=start
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0x100000000
+        max_int = 0x7FFFFFFF
+        min_int = max_int + 1
+
+        while b != 0:
+            carry = (a & b) << 1
+            a = (a ^ b) % mask
+            b = carry % mask
+
+        return a if a <= max_int else ~((a % min_int) ^ max_int)
+
+    # @lc code=end
