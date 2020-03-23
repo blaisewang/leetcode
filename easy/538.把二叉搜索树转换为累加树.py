bccbca95092d82bd127d2ccbf683d4f678400c4c
Blaise Wang <kaito.kaito.k@gmail.com>
2020-03-23 20:41:08 +0800

    Solve 532, 538, 541, 543, 551, 557, 559, 561

---
+++ easy/538.把二叉搜索树转换为累加树.py
@@ -0,0 +1,63 @@
+#
+# @lc app=leetcode.cn id=538 lang=python3
+#
+# [538] 把二叉搜索树转换为累加树
+#
+# https://leetcode-cn.com/problems/convert-bst-to-greater-tree/description/
+#
+# algorithms
+# Easy (57.52%)
+# Likes:    217
+# Dislikes: 0
+# Total Accepted:    19.4K
+# Total Submissions: 32.5K
+# Testcase Example:  '[5,2,13]'
+#
+# 给定一个二叉搜索树（Binary Search Tree），把它转换成为累加树（Greater
+# Tree)，使得每个节点的值是原来的节点值加上所有大于它的节点值之和。
+# 
+# 
+# 
+# 例如：
+# 
+# 输入: 原始二叉搜索树:
+# ⁠             5
+# ⁠           /   \
+# ⁠          2     13
+# 
+# 输出: 转换为累加树:
+# ⁠            18
+# ⁠           /   \
+# ⁠         20     13
+# 
+# 
+# 
+# 
+# 注意：本题和 1038:
+# https://leetcode-cn.com/problems/binary-search-tree-to-greater-sum-tree/ 相同
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def __init__(self):
+        self.total = 0
+
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        if root is not None:
+            self.convertBST(root.right)
+            self.total += root.val
+            root.val = self.total
+            self.convertBST(root.left)
+
+        return root
+
+# @lc code=end
---
+++ easy/559.n叉树的最大深度.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode.cn id=559 lang=python3
+#
+# [559] N叉树的最大深度
+#
+# https://leetcode-cn.com/problems/maximum-depth-of-n-ary-tree/description/
+#
+# algorithms
+# Easy (67.47%)
+# Likes:    77
+# Dislikes: 0
+# Total Accepted:    18.3K
+# Total Submissions: 26.6K
+# Testcase Example:  '[1,null,3,2,4,null,5,6]'
+#
+# 给定一个 N 叉树，找到其最大深度。
+# 
+# 最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。
+# 
+# 例如，给定一个 3叉树 :
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 我们应返回其最大深度，3。
+# 
+# 说明:
+# 
+# 
+# 树的深度不会超过 1000。
+# 树的节点总不会超过 5000。
+# 
+#
+
+# @lc code=start
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def maxDepth(self, root: Node) -> int:
+        if root is None:
+            return 0
+        elif not root.children:
+            return 1
+        else:
+            height = [self.maxDepth(c) for c in root.children]
+            return max(height) + 1
+
+# @lc code=end
