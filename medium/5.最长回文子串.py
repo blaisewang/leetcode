b96383bfb9109120c803cf38d33f27775aa89ef4
Blaise Wang <kaito.kaito.k@gmail.com>
2020-07-06 12:57:17 +0800

    solve 6

---
+++ medium/5.最长回文子串.py
@@ -0,0 +1,53 @@
+#
+# @lc app=leetcode.cn id=5 lang=python3
+#
+# [5] 最长回文子串
+#
+# https://leetcode-cn.com/problems/longest-palindromic-substring/description/
+#
+# algorithms
+# Medium (27.78%)
+# Likes:    2391
+# Dislikes: 0
+# Total Accepted:    308.9K
+# Total Submissions: 997.5K
+# Testcase Example:  '"babad"'
+#
+# 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+# 
+# 示例 1：
+# 
+# 输入: "babad"
+# 输出: "bab"
+# 注意: "aba" 也是一个有效答案。
+# 
+# 
+# 示例 2：
+# 
+# 输入: "cbbd"
+# 输出: "bb"
+# 
+# 
+#
+
+
+# @lc code=start
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expand_from_center(l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return l + 1, r - 1
+
+        start, end = 0, 0
+        for i in range(len(s)):
+            l1, r1 = expand_from_center(i, i)
+            l2, r2 = expand_from_center(i, i + 1)
+            if r1 - l1 > end - start:
+                start, end = l1, r1
+            if r2 - l2 > end - start:
+                start, end = l2, r2
+        return s[start: end + 1]
+
+# @lc code=end
